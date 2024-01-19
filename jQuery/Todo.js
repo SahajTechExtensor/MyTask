1e12ad705594f08d3ae126caa6cd0e577945aac8
Sahaj Mansuri <156072621+SahajTechExtensor@users.noreply.github.com>
2024-01-19 12:12:55 +0000

    update toDoList

--- jQuery/Todo.js
+++ jQuery/Todo.js
@@ -1,14 +1,31 @@
 // alert("hello");
 
 $(document).ready(function(){
+
+
     $(".btn").click(function(){
         var ToList = $('input[name=Task]').val();
-        $("ul").append("<li>"+ToList+"</li>")
+        if(ToList){
+            $("ul").append("<li>"+ToList+"</li>")
+        }
+        $('input[name=Task]').val(undefined);
 
     })
-    $(document).on('dblclick','li',function(){
-        $(this).toggleClass('.del').fadeOut('slow');
+
+
+    
+    $(document).on('click','li',function(){
+        console.log(li)
+        $('input[name=Task]').val();
     })
+
+
+
+    $(document).on('dblclick','li',function(){
+        // $(this).toggleClass('.del').remove();
+         $(this).toggleClass('.del').fadeOut('slow');
+    })
+
 
 
 });
